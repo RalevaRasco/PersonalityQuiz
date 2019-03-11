9390c94cb0ea53bb89d0fb5a6de7aefe924bbe34
Oscar <oscara@csedge.org>
2019-03-11 01:17:18 -0700

    bn

--- script.js
+++ script.js
@@ -1,2 +1,41 @@
-let hi = "hello"
-console.log(hi);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//point system
+let secretPoints = {
+     no: 1,
+     kinda: 2,
+     ye: 3,
+     no2: 0
+}
+
+let hobbiePoints = {
+     : 0,
+     : 1,
+     : 3,
+     : 2
+}
+
+let moneyPoints = {
+    : 2,
+    : 1,
+    : 0,
+    : 3
+}
